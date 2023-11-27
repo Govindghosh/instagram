82c3d405f9d5f4f001e64f0f4886368e27f8fee4
Govind Ghosh <130273101+Govindghosh@users.noreply.github.com>
2023-11-27 11:42:51 +0530

    create and add env file

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,12 +1,9 @@
-const conf={
-    
-    appwURL: String(import.meta.env.VITE_APPWRITE_URL),
-    appwPrID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwDbID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwCollID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwBucID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-
-
+const conf = {
+    Url: String(import.meta.env.VITE_APPWRITE_URL),
+    ProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    DatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    CollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    BucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
 export default conf;
