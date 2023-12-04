8187eac594729a3da101a945bf8f5d51982ec1d3
Govind Ghosh <130273101+Govindghosh@users.noreply.github.com>
2023-12-04 16:51:04 +0530

    index.jsx made

---
+++ src/components/Index.js
@@ -0,0 +1,22 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer' ;
+import Container from './container/Container';
+import LogoutBtn from './Footer/LogoutBtn';
+import Button from './Button';
+import Postform from './Post-Form/Postform';
+import AuthLayout from './AuthLayout';
+
+
+
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    Button,
+    Postform,
+    AuthLayout
+
+}
