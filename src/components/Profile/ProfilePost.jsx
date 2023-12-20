8158237c91007c7c1514f5550bb3492206e9e3b3
Govind Ghosh <130273101+Govindghosh@users.noreply.github.com>
2023-12-20 10:21:44 +0530

    work  on ProfilePost on image Manage

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { Flex, GridItem, Image, Text } from '@chakra-ui/react';
+import { AiFillHeart } from 'react-icons/ai';
+import { FaComment } from 'react-icons/fa';
 
-function ProfilePost() {
+function ProfilePost({ Img }) {
   return (
-    <>
-        <h1>ProfilePost</h1> 
-    </>
-  )
+    <GridItem
+      cursor='pointer'
+      borderRadius={2}
+      overflow='hidden'
+      border='1px solid'
+      borderColor='whiteAlpha.300'
+      position='relative'
+      aspectRatio={1 / 1}
+      _hover={{ '.overlay': { display: 'flex' } }}
+    >
+      <Flex
+        display='none'
+        position='absolute'
+        bg='blackAlpha.700'
+        transition='all 0.3s ease'
+        top={0}
+        right={0}
+        bottom={0}
+        left={0}
+        zIndex={1}
+        justifyContent='center'
+        className='overlay'
+      >
+        <Flex alignItems='center' justifyContent='center' gap={50}>
+          <Flex>
+            <AiFillHeart size={20} />
+            <Text fontWeight='bold' ml={2}>
+              7
+            </Text>
+          </Flex>
+          <Flex>
+            <FaComment size={20} />
+            <Text fontWeight='bold' ml={2}>
+              7
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Image src={Img} alt='pro' objectFit='cover' w='100%' h='100%' />
+    </GridItem>
+  );
 }
 
-export default ProfilePost
+export default ProfilePost;
--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import { Grid, GridItem } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Box, Grid, Skeleton, VStack } from '@chakra-ui/react';
+import ProfilePost from './ProfilePost';
 
 function ProfilePosts() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a data fetch
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <>
-      <Grid templateColumns='repeat(3, 1fr)' gap={1}>
-        <GridItem w='100%' h={{}} bg='blue.500' />
-        <GridItem w='100%' h={{}} bg='blue.500' />
-        <GridItem w='100%' h={{}} bg='blue.500' />
-        <GridItem w='100%' h={{}} bg='blue.500' />
-        <GridItem w='100%' h={{}} bg='blue.500' />
-      </Grid>
-    </>
+    <Grid templateColumns='repeat(3, 1fr)' gap={1}>
+      {isLoading ? (
+        [0, 1, 2, 3, 4, 5].map((_, idx) => (
+          <VStack key={idx} alignItems='flex-start' gap={1}>
+            <Skeleton w='full'>
+              <Box h='300px'></Box>
+            </Skeleton>
+          </VStack>
+        ))
+      ) : (
+        [0, 1, 2].map((index) => (
+          <ProfilePost key={index} img="https://filmfare.wwmindia.com/photogallery/2023/may/salmankhan11684830563.jpg" />
+        ))
+      )}
+    </Grid>
   );
 }
 
 export default ProfilePosts;
-
