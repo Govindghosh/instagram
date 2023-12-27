9b5e34bf666bdf2e4a2e782a3690949018601501
Govind ghosh <govindghosh0@gmail.com>
2023-12-28 00:07:38 +0530

    here is singup components connected to firebare and navigate to /home

--- src/Hook/useSignupWithEmailAndPassword.js
+++ src/Hook/useSignupWithEmailAndPassword.js
@@ -1,70 +1,63 @@
-import { collection, doc, query, setDoc, where, getDocs } from "firebase/firestore";
-import { auth, firestore } from "../Firebase/firebaseConfig";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../Firebase/firebaseConfig";
+import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
 import useShowToast from "./useShowToast";
-import authSlice from "../store/authSlice";
+import { useNavigate } from "react-router-dom";
+//import useAuthStore from "../store/authStore";
 
+const useSignUpWithEmailAndPassword = () => {
+	const [createUserWithEmailAndPassword, , loading, error] = useCreateUserWithEmailAndPassword(auth);
+	const showToast = useShowToast();
+	//const loginUser = useAuthStore((state) => state.login);
 
+	const signup = async (inputs) => {
+		
+		if (!inputs.email || !inputs.password || !inputs.username || !inputs.fullName) {
+			showToast("Error", "Please fill all the fields", "error");
+			return;
+		}
 
-const useSignupWithEmailAndPassword = () => {
-    // const loginUser = authSlice((state)=> state.login);
-    // const logoutUser = authSlice((state)=> state.logout);
-    const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
-    const showToast = useShowToast()
-   
-    const signup = async (inputs) => {
-      if (!inputs.email || !inputs.username || !inputs.fullName || !inputs.password) {
-        showToast("Error", "Please fill all the fields", "error");
-        return;
-      }
+		const usersRef = collection(firestore, "users");
 
-      const usersRef = collection(firestore, "users");
-      const q = query(usersRef, where("username", "==", inputs.username));
-      const querySnapshot = await getDocs(q);
-      if (!querySnapshot.empty) {
-        showToast("Error", "username already taken", "error");
-        return
-      }
-      
-  
-      try {
-        const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
-  
-        if (!newUser && error) {
-          showToast("Error", error, "error");
-          return;
-        }
-  
-        if (newUser) {
-          const userDoc = {
-            uid: newUser.user.uid,
-            email: inputs.email,
-            username: inputs.username,
-            fullName: inputs.fullName,
-            bio: "",
-            profilePicURL: "",
-            followers: [],
-            following: [],
-            posts: [],
-            createAt: Date.now(),
-          };
-  
-          await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
-          localStorage.setItem("user-info", JSON.stringify(userDoc));
-          // loginUser(userDoc);
-        }
-      } catch (error) {
-        showToast("Error", error.message, "error")
-      }
-    };
-  
+		const q = query(usersRef, where("username", "==", inputs.username));
+		const querySnapshot = await getDocs(q);
 
-  return {
-    signup,
-    loading,
-    error,
-  }
-}
+		if (!querySnapshot.empty) {
+			showToast("Error", "Username already exists", "error");
+			return;
+		}
 
-export default useSignupWithEmailAndPassword;
+		try {
+			const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
+			if (!newUser && error) {
+				showToast("Error", error.message, "error");
+				return;
+			}
+			if (newUser) {
+				const userDoc = {
+					uid: newUser.user.uid,
+					email: inputs.email,
+					username: inputs.username,
+					fullName: inputs.fullName,
+					bio: "",
+					profilePicURL: "",
+					followers: [],
+					following: [],
+					posts: [],
+					createdAt: Date.now(),
+				};
+				await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+				localStorage.setItem("user-info", JSON.stringify(userDoc));
+				
 
+				//loginUser(userDoc);
+			}
+		} catch (error) {
+			showToast("Error", error.message, "error");
+		}
+	};
+
+	return { loading, error, signup };
+};
+
+export default useSignUpWithEmailAndPassword;
--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -7,99 +7,94 @@ import useSignupWithEmailAndPassword from '../../Hook/useSignupWithEmailAndPassw
 
 
 
-
-
 function Signup() {
-    const navigate = useNavigate();
-    const [show, setShow] = useState(false);
-    const handleClick = () => setShow(!show);
-    const { loading, error, signup } = useSignupWithEmailAndPassword();
-    // const handelSignup = ()=>{
-    //   console.log("inputs", inputs)
-    //   if (!inputs.email || !inputs.password) {
-    //     alert("please enter you Email and Password");
-    //   } else {
-    //     navigate("/home")
-    //     }
-    // };
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  const { loading, error, signup } = useSignupWithEmailAndPassword();
 
-    const [inputs, setInputs]=useState({
-      fullName: "",
-      username: "",
-      email: "",
-      password: ""
-    })
+  const handleSignup = async (inputs) => {
+    await signup(inputs);
+    navigate('/home');
+  };
+
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    email: "",
+    password: ""
+  })
 
   return (
     <>
       <Input 
-                    type="email"
-                    value={Input.email}
-                    onChange={(e)=>setInputs({...inputs,email:e.target.value})}
-                    placeholder="Mobile Number or Email"
-                    fontSize={14}/>
-                 <Input
-                    type="text" 
-                    placeholder="Username"
-                    value={Input.username}
-                    fontSize={14}  // Corrected here
-                    onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-                />
-                <Input
-                    type="text" 
-                    placeholder="Full Name"
-                    value={Input.fullName}
-                    fontSize={14}  // Corrected here
-                    onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
-                />
-                <InputGroup>
-                <Input
-                    type={show ? "text" : "password"}
-                    placeholder="Password"
-                    value={Input.password}
-
-                    fontSize={14}  // Corrected here
-                    onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-                />
-
-                    <InputRightElement>
-                    <Button h='1.75rem' size='sm' onClick={handleClick}>
-                     {show ? <IoIosEyeOff /> : <IoMdEye />}
-                    </Button>
-                    </InputRightElement>
-                </InputGroup>
-                {error &&(
-                    <Alert status='error' p={2} fontSize={13} borderRadius={4}>
-                    <AlertIcon fontSize={12} />
-                    {error.message}
-                  </Alert>
-                )}
-                <Box
-                className="text-sm text-gray-600 text-center"
-                maxW={350}
-                padding={2}
-              >
-                People who use our service may have uploaded your contact
-                information to Instagram. Learn More
-              </Box>
-              <Box
-                className="text-sm text-gray-600 text-center"
-                maxW={350}
-                padding={2}
-              >
-                By signing up, you agree to our Terms , Privacy Policy and
-                Cookies Policy .
-              </Box>
-              <WrapItem>
-                <Button className="w-64" colorScheme="linkedin"
-                isLoading={loading}
-                onClick={()=>signup(inputs)}
-                  >
-                  Sign Up
-                </Button>
-              </WrapItem>
+        type="email"
+        value={inputs.email}
+        onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+        placeholder="Mobile Number or Email"
+        fontSize={14}
+      />
+      <Input
+        type="text" 
+        placeholder="Username"
+        value={inputs.username}
+        fontSize={14}
+        onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+      />
+      <Input
+        type="text" 
+        placeholder="Full Name"
+        value={inputs.fullName}
+        fontSize={14}
+        onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+      />
+      <InputGroup>
+        <Input
+          type={show ? "text" : "password"}
+          placeholder="Password"
+          value={inputs.password}
+          fontSize={14}
+          onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+        />
+        <InputRightElement>
+          <Button h='1.75rem' size='sm' onClick={handleClick}>
+            {show ? <IoIosEyeOff /> : <IoMdEye />}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      {error && (
+        <Alert status='error' p={2} fontSize={13} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+      <Box
+        className="text-sm text-gray-600 text-center"
+        maxW={350}
+        padding={2}
+      >
+        People who use our service may have uploaded your contact information to Instagram. Learn More
+      </Box>
+      <Box
+        className="text-sm text-gray-600 text-center"
+        maxW={350}
+        padding={2}
+      >
+        By signing up, you agree to our Terms, Privacy Policy, and Cookies Policy.
+      </Box>
+      <WrapItem>
+        <Button
+          className="w-64"
+          colorScheme="linkedin"
+          isLoading={loading}
+          onClick={() => handleSignup(inputs)}
+          //onClick={() => signup(inputs)}
+        >
+          Sign Up
+        </Button>
+      </WrapItem>
     </>
   )
 }
 
-export default Signup
+export default Signup;
