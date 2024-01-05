0a46695cf78df9b4ba16c1243e4546114bce765a
Govind ghosh <govindghosh0@gmail.com>
2024-01-05 16:40:34 +0530

    [FIX]: useLogout.jsx

--- src/Hook/useLogout.js
+++ src/Hook/useLogout.js
@@ -3,17 +3,20 @@ import { auth } from "../Firebase/firebaseConfig";
 import useShowToast from "./useShowToast";
 import { logout } from "../store/authSlice";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const useLogout = () => {
   const dispatch = useDispatch();
   const [signOut, isLoggingOut, error] = useSignOut(auth);
   const showToast = useShowToast();
+  const navigate = useNavigate();
 
   const handelLogout = async () => {
     try {
       await signOut();
       localStorage.removeItem("user-info");
       dispatch(logout());
+      navigate("/");
     } catch (error) {
       showToast("Error", error.message, "error");
     }
