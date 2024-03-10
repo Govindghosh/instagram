bf48ccba0e01dd4d862cae2d8424cb1fe2c18997
Govind ghosh <govindghosh0@gmail.com>
2024-03-10 18:15:44 +0530

    [FIX]SuggestedHeader.jsx

--- src/components/Layout/Suggested/SuggestedHeader.jsx
+++ src/components/Layout/Suggested/SuggestedHeader.jsx
@@ -1,31 +1,38 @@
-import { Avatar, Flex, Link, Text } from "@chakra-ui/react";
+import { Avatar, Flex, Button, Text } from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link as RouterLink } from "react-router-dom";
+//import { Link as RouterLink } from "react-router-dom";
+import useLogout from "../../../Hook/useLogout";
 
 function SuggestedHeader() {
   const authUser = useSelector((state) => state.auth.user);
+  const { handelLogout, isLoggingOut } = useLogout();
 
   return (
     <>
       <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
         <Flex alignItems={"center"} gap={2}>
-          <Avatar src={authUser?.profilePicURL} name={authUser?.fullName}></Avatar>
+          <Avatar
+            src={authUser?.profilePicURL}
+            name={authUser?.fullName}
+          ></Avatar>
           <Text fontWeight={"bold"} fontSize={14}>
-          {authUser?.fullName}
+            {authUser?.fullName}
           </Text>
         </Flex>
-        <Link
+        <Button
           className="no-underline"
-          as={RouterLink}
           color={"blue.600"}
           fontSize={12}
           fontWeight={"bold"}
-          to={"/"}
+          onClick={handelLogout}
           style={{ textDecoration: "none" }}
+          isLoading={isLoggingOut}
+          bg={"transparent"}
+          _hover={{ bg: "transparent" }}
         >
           Switch
-        </Link>
+        </Button>
       </Flex>
     </>
   );
